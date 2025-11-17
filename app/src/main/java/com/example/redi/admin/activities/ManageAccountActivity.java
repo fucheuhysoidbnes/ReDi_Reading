31690261a0d1ef4532bcf08cf516894fe672ac43
fucheuhysoidbnes <dangt9992@gmail.com>
2025-11-17 10:41:12 +0700

    slide left right cho quản lý tài khoản

--- app/src/main/java/com/example/redi/admin/activities/ManageAccountActivity.java
+++ app/src/main/java/com/example/redi/admin/activities/ManageAccountActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.redi.R;
 import com.example.redi.common.base.BaseAdminActivity;
@@ -30,17 +30,21 @@ public class ManageAccountActivity extends BaseAdminActivity {
     }
 
     public void openFragment(Fragment f, boolean addToBackstack) {
-        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(
+                R.anim.slide_in_right,
+                R.anim.slide_out_left,
+                R.anim.slide_in_right,
+                R.anim.slide_out_left
+        );
+
+        ft.replace(R.id.admin_fragment_container, f);
+
         if (addToBackstack) {
-            fm.beginTransaction()
-                    .replace(R.id.admin_fragment_container, f)
-                    .addToBackStack(null)
-                    .commit();
-        } else {
-            fm.beginTransaction()
-                    .replace(R.id.admin_fragment_container, f)
-                    .commit();
+            ft.addToBackStack(null);
         }
+
+        ft.commit();
     }
 
 }
